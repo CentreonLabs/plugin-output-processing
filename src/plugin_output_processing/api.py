c1976d3fd0b7cb579d493f007e4d5c5575e2c7dd
Denis Roussel <droussel@centreon.com>
2024-06-11 10:49:04 -0400

    feat: Add a new endpoint explain in the API

--- src/plugin_output_processing/api.py
+++ src/plugin_output_processing/api.py
@@ -17,6 +17,11 @@ def get_prompt(
     name: str = "n/a",
     description: str = "n/a",
 ):
+    """Build a prompt to be send to a LLM.
+
+    This endpoint is useful if the prompt needs to be modified by the end user before
+    being sent to the LLM.
+    """
     uuid = uuid4()
     prompt = processor.get_prompt(type, output, name, description, uuid)
     return prompt, uuid
@@ -24,6 +29,20 @@ def get_prompt(
 
 @app.get("/send")
 def send_prompt(prompt: str, uuid: UUID):
+    return processor.send_prompt(prompt, uuid)
 
-    response = processor.send_prompt(prompt, uuid)
-    return response
+
+@app.get("/explain")
+def explain(
+    type: Literal["host", "service"],
+    output: str = "n/a",
+    name: str = "n/a",
+    description: str = "n/a",
+):
+    """Get an explanation for the output.
+
+    This is a combination of the get and send endpoints.
+    """
+    uuid = uuid4()
+    prompt = processor.get_prompt(type, output, name, description, uuid)
+    return processor.send_prompt(prompt, uuid)
