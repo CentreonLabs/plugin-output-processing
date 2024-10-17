49a5f28195e0376dbd96a1af177f78722589cdaa
gregory-lvtx <gleventoux@centreon.com>
2024-10-17 14:42:14 +0200

    Start API with poetry script pop

--- src/plugin_output_processing/api.py
+++ src/plugin_output_processing/api.py
@@ -14,6 +14,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import uvicorn
 from typing import Literal
 from uuid import UUID, uuid4
 
@@ -63,3 +64,7 @@ def explain(
     uuid = uuid4()
     prompt = processor.get_prompt(type, name, output, description, uuid)
     return processor.send_prompt(prompt, uuid)
+
+
+def main():
+    uvicorn.run(app)
