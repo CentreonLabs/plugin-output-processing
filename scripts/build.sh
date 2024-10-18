d17c6a08f09112c3a9955a9bcdfcf3c662ec0355
gregory-lvtx <gleventoux@centreon.com>
2024-10-18 10:10:12 +0200

    Create script to build docker image

---
+++ scripts/build.sh
@@ -0,0 +1,16 @@
+# Name of the project
+PROJECT=pop
+
+# Registery where images are pushed
+REGISTRY=centreonlabs
+
+# Images are tagged with the project version
+TAG=$(poetry version -s)
+
+# Build the image
+DOCKERFILE=docker/Dockerfile
+IMAGE="$REGISTRY/$PROJECT:$TAG"
+echo
+echo -e "Building image \e[34m$IMAGE\e[0m ..."
+echo
+docker build -t $IMAGE -f $DOCKERFILE .
