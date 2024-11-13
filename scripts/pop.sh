26f239616798ecc7e0c48ed3d27b649e267a231d
Thomas Arnaud <tarnaud@centreon.com>
2024-11-13 10:12:55 +0100

    add pop querier bash script and prefere usage of docker-compose than docker compose which isn-t available everywhere yet

---
+++ scripts/pop.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# This script prompts the user to input plugin output text, URL encodes the input,
+# and sends it to a specified API endpoint for explanation. The response from the
+# API is then formatted and displayed to the user.
+
+# Prompt the user to input text
+read -p "Enter plugin output you want to be explained: " input_text
+
+echo
+echo "----------"
+echo
+
+# URL encode the input text
+encoded_text=$(echo -n "$input_text" | jq -s -R -r @uri)
+
+# Define the API endpoint, replacing YOUR_API_ENDPOINT with the actual endpoint
+api_endpoint="http://127.0.0.1:8000/explain"
+
+# Make the GET request
+response=$(curl -s "${api_endpoint}?type=service&output=${encoded_text}")
+
+# replace return carriage and display the response
+echo $response | sed 's/\\n/\n/g'
